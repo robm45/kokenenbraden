465b55b48105ed3ebd1b27ed4f6052c68028c386
Rob Kok <rob@casakok.nl>
2026-01-18 13:32:19 +0100

    taal op nederlands gezet

--- config/settings.py
+++ config/settings.py
@@ -164,13 +164,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'nl'
 TIME_ZONE = 'Europe/Amsterdam'
-
 USE_I18N = True
-
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
