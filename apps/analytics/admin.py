5a79c635b9a6192eb1f7a7adc4066f6c389a8123
Rob Kok <rob@casakok.nl>
2026-01-19 09:46:59 +0100

    reset analytics toegevoegd

--- apps/analytics/admin.py
+++ apps/analytics/admin.py
@@ -1,15 +1,47 @@
 from django.contrib import admin
 from .models import DailyVisit
 from .models import ReceptViewCount
+from django.urls import path
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 @admin.register(DailyVisit)
 class DailyVisitAdmin(admin.ModelAdmin):
     list_display = ("date", "count")
     ordering = ("-date",)
+    change_list_template = "admin/analytics/reset_analytics_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "reset-analytics/",
+                self.admin_site.admin_view(self.reset_analytics),
+                name="reset-analytics",
+            ),
+        ]
+        return custom_urls + urls
+
+    def reset_analytics(self, request):
+        if request.method == "POST":
+            DailyVisit.objects.all().delete()
+            ReceptViewCount.objects.all().delete()
+
+            self.message_user(
+                request,
+                "Analytics zijn succesvol gereset.",
+                messages.SUCCESS,
+            )
+            return redirect("..")
+
+        return render(
+            request,
+            "admin/analytics/reset_analytics_confirm.html",
+        )
 
 @admin.register(ReceptViewCount)
 class ReceptViewCountAdmin1(admin.ModelAdmin):
     list_display = ("recept", "count")
     ordering = ("-count",)
-
 
